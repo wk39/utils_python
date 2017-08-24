98485d2b31d1e57757d75d92052364a2933b5eb7
wk39 <wook3910@gmail.com>
2017-08-24 23:36:34 +0900

    u_opencv funtion names changed

--- u_opencv.py
+++ u_opencv.py
@@ -1,19 +1,30 @@
 import numpy as np
 import cv2
 
-def draw_lines_pair(img, pts, color,linewidth=1):
+def lines_pair(img, pts, color, linewidth=1):            # draw_lines_pair
     ''' draw lines between pairs of points 
     ex) for 2*n points input, draw n lines
+
+    img - input image (numpy array)
+    pts - image points numpy array sized 2+xN
+    color - bgr integer tuple
+
+    return - image
     '''
     for i in range(0,pts.shape[1],2):
         p0 = tuple(pts[0:2,i  ].astype(np.float32))
         p1 = tuple(pts[0:2,i+1].astype(np.float32))
         img = cv2.line(img, p0, p1, color, linewidth)
-    return img
 
 
-def draw_cross_point(img, pts, mark_size, color):
-    ''' draw cross mark line (+) not (x)
+def cross_mark(img, pts, mark_size, color, linewidth=1): # draw_cross_point
+    ''' draw cross mark line (+)
+
+    img - input image (numpy array)
+    pts - image points numpy array sized 2+xN
+
+    return - image
+
     '''
     w = [mark_size/2,0]
     h = [0,mark_size/2]
@@ -22,10 +33,13 @@ def draw_cross_point(img, pts, mark_size, color):
     pt = pts[0:2,0]-h
     pb = pts[0:2,0]+h
 
-    cv2.line(img, tuple(pl.astype(np.float32)), tuple(pr.astype(np.float32)), color, 1)
-    cv2.line(img, tuple(pt.astype(np.float32)), tuple(pb.astype(np.float32)), color, 1)
+    img = cv2.line(img, tuple(pl.astype(np.float32)), tuple(pr.astype(np.float32)), color, linewidth)
+    img = cv2.line(img, tuple(pt.astype(np.float32)), tuple(pb.astype(np.float32)), color, linewidth)
 
-def rect2points(r, as_int=True):
+    return img
+
+
+def rect_to_points(r, as_int=True):
 
     x,y,w,h = r
     if as_int:
@@ -37,8 +51,10 @@ def rect2points(r, as_int=True):
 
     return p1, p2
 
+
 def generate_grid_lines( xi, yi):#, A):
     ''' generate grid lines (numpy)
+
     xi - x-dir [min, max, res]
     yi - y-dir [min, max, res]
     '''
@@ -58,30 +74,31 @@ def generate_grid_lines( xi, yi):#, A):
     return pg#, dg
 
 
-def convert_world_to_image_points(K,p):
+def xyz1_to_uv1(K,xyz1): #convert_world_to_image_points
     ''' convert xyz coordinate to uv(image) coordinate
-    K - camera matrix ( K = P * [R|t] )
-    p - 4xn vectors - ex) one of columns has [x,y,z,1].T
+    K    - camera matrix ( K = P * [R|t] )
+    xyz1 - 4xn vectors - ex) one of columns has [x,y,z,1].T
 
-    return - 4xn vectors - ex one of columns has [u,v,1].T
+    return - 3xn vectors - ex one of columns has [u,v,1].T
     '''
-    pp = np.matmul(K,p)
+    pp = np.matmul(K,xyz1)
     return pp/pp[2]
 
-def convert_image_to_world_in_ground_points(Kmi,p):
+
+def uv1_to_xy01(Kmi,uv1):  # convert_image_to_world_in_ground_point
     ''' convert image points (uv) to world coordinate in ground (xy0) 
     (assume image points are on the ground
 
     Kmi - modified inverse camera matrix
          Km - modified camera matrix K with 3rd column removed, hence 3x3
          kmi - inverse of Km
-    p - 3xn vectors - ex) one of columns has [u,v,1].T
+    uv1 - 3xn vectors - ex) one of columns has [u,v,1].T
 
     return - 4xn vectors - ex one of columns has [x,y,0,1]
     '''
     S = np.array([[1,0,0],[0,1,0],[0,0,0],[0,0,1]])
     K = np.matmul( S, Kmi)
-    pp = np.matmul(K,p)
+    pp = np.matmul(K,uv1)
     return pp/pp[3]
 
 

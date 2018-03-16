ea582a4a381eae2112eb6bd4df0bd25d51a8bca9
wk39 <wook3910@gmail.com>
2018-03-17 00:15:06 +0900

    * u_coordinate - add docstrings, test functions for new interfaces

--- u_coordinate.py
+++ u_coordinate.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+
 import numpy as np
 
 # print(np.__version__)
@@ -34,9 +36,252 @@ import numpy as np
 '''
 
 
+
+
+''' New Interfaces 
+    2017.12.08
+    TODO: add test funtions
+'''
+
+def RotationMatrixFromAxisAngle(axis, theta):
+    ''' 
+    parameter:
+
+        axis  [ string ] - 'x', 'y', 'z'
+        theta [ float  ] - angle in radian
+
+    return:
+
+        R [ numpy array 3x3 ] - rotation matrix
+
+    description:
+        
+        Rotation matrix 'R' means as below
+
+        v_world = R * v_body
+
+        [x]              =  [         ]                   [x]
+        [y]              =  [    R    ]                *  [y]
+        [z] world frame  =  [         ] body to world     [z] body frame
+
+    '''
+
+    if   axis=='x':
+        return np.array([
+            [  1,              0,              0 ],
+            [  0,  np.cos(theta), -np.sin(theta) ],
+            [  0,  np.sin(theta),  np.cos(theta) ]])
+    elif axis=='y':
+        return np.array([
+            [  np.cos(theta), 0,  np.sin(theta) ],
+            [              0, 1,              0 ],
+            [ -np.sin(theta), 0,  np.cos(theta) ]])
+    elif axis=='z':
+        return np.array([
+            [  np.cos(theta), -np.sin(theta), 0 ],
+            [  np.sin(theta),  np.cos(theta), 0 ],
+            [              0,              0, 1 ]])
+    else:
+        raise Exception('invalid axis value : '+str(axis))
+
+
+def RotationMatrixFromEulerAngles(r,p,y):
+    ''' 
+    parameter:
+
+        r [ float ] - roll  in radian
+        p [ float ] - pitch in radian
+        y [ float ] - yaw   in radian
+
+    return:
+
+        R [ numpy array 3x3 ] - rotation matrix
+
+    description:
+        
+        Rotation matrix 'R' means as below
+
+        v_world = R * v_body
+
+        [x]              =  [         ]                   [x]
+        [y]              =  [    R    ]                *  [y]
+        [z] world frame  =  [         ] body to world     [z] body frame
+
+    '''
+    return np.dot( RotationMatrixFromAxisAngle('z', y),
+            np.dot( RotationMatrixFromAxisAngle('y', p),
+                RotationMatrixFromAxisAngle('x', r)))
+
+def RotationMatrixFromQuaternion(q):
+    ''' 
+    parameter:
+
+        q [ array 4 ] - quaternion in (w,x,y,z) order
+
+    return:
+
+        R [ numpy array 3x3 ] - rotation matrix
+
+    description:
+
+        https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
+
+    '''
+    r,i,j,k = q
+
+    return np.array([
+        [ 1-2*j*j-2*k*k, 2*(i*j-k*r), 2*(i*k+j*r) ],
+        [ 2*(i*j+k*r), 1-2*i*i-2*k*k, 2*(j*k-i*r) ],
+        [ 2*(i*k-j*r), 2*(i*r+j*k), 1-2*i*i-2*j*j ]])
+
+
+def QuaternionFromRotationMatrix(R):
+    ''' 
+    parameter:
+
+        R [ numpy array 3x3 ] - rotation matrix
+
+    return:
+
+        q [ array 4 ] - quaternion in (w,x,y,z) order
+
+
+    description:
+
+        https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
+    '''
+
+    r = 0.5*np.sqrt(1.0+R[0,0]+R[1,1]+R[2,2])
+    i = (R[2,1]-R[1,2])/(4.0*r)
+    j = (R[0,2]-R[2,0])/(4.0*r)
+    k = (R[1,0]-R[0,1])/(4.0*r)
+
+    return np.array([r,i,j,k])
+
+
+def QuaternionFromAxisAngle(v, theta):
+    ''' 
+    parameter:
+
+        v     [ array 3 ] - rotation axis as unit vector
+        theta [ float   ] - rotation angle in radian
+
+    return:
+
+        q [ array 4 ] - quaternion in (w,x,y,z) order
+
+    '''
+    q = np.zeros(4)
+
+    q[0] = np.cos(theta/2)
+    q[1] = np.sin(theta/2) * v[0]
+    q[2] = np.sin(theta/2) * v[1]
+    q[3] = np.sin(theta/2) * v[2]
+
+    return q
+
+
+def AxisAngleFromQuaternion(q):
+    ''' 
+    parameter:
+
+        q [ array 4 ] - quaternion in (w,x,y,z) order
+
+    return:
+
+        unit_vector [ array 3 ] - rotation axis as unit vector
+        theta       [ float   ] - rotation angle in radian
+
+    '''
+    if abs(q[0]-1.0) < 1e-8:
+        unit_vector = np.array([0.0,0.0,1.0])
+        theta       = 0.0
+    else:
+        theta_d2 = np.arccos(q[0])
+        v = q[1:]/np.sin(theta_d2)
+
+        unit_vector = v / np.linalg.norm(v)
+        theta       = theta_d2*2.0
+
+    return unit_vector, theta
+
+
+def EulerAnglesFromRotationMatrix(R):
+    ''' 
+    parameter:
+
+        R [ numpy array 3x3 ] - rotation matrix
+
+    return:
+
+        rpy [ numpy array 3 ] - (roll, pitch, yaw) order in radian
+
+    description:
+    
+        https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+    '''
+ 
+    sy = np.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+     
+    singular = sy < 1e-6
+ 
+    if  not singular :
+        r = np.arctan2(R[2,1] , R[2,2])
+        p = np.arctan2(-R[2,0], sy)
+        y = np.arctan2(R[1,0], R[0,0])
+    else :
+        r = np.arctan2(-R[1,2], R[1,1])
+        p = np.arctan2(-R[2,0], sy)
+        y = 0
+ 
+    return np.array([r, p, y])
+
+
+def TransformationMatrix(r,p,y, tx,ty,tz):
+    ''' 
+    parameter:
+
+        r  [ float ] - roll  in radian
+        p  [ float ] - pitch in radian
+        y  [ float ] - yaw   in radian
+        tx [ float ] - x translation
+        ty [ float ] - y translation
+        tz [ float ] - z translation
+
+    return:
+
+        T [ numpy array 4x4 ] - transformation matrix
+
+            [ R11 R12 R13   tx]
+            [ R21 R22 R23   ty]
+            [ R31 R32 R33   tz]
+            [   0   0   0   1 ]
+
+
+    '''
+    T = np.zeros((4,4))
+    T[0,3] = tx
+    T[1,3] = ty
+    T[2,3] = tz
+    T[3,3] = 1.0
+    T[0:3,0:3] = RotationMatrixFromEulerAngles(r,p,y)
+
+    return T
+
+
+
+
+
+
+
+
+
+
+
 def Rxyz(theta, axis):
 
-    ''' Rotation matrix
+    '''Deprecated ...
+       Rotation matrix
        theta [radian]
        axis - 'x','y','z'
     '''
@@ -62,7 +307,8 @@ def Rxyz(theta, axis):
 
 def Rrpy(r,p,y):
 
-    ''' Rotation matrix for Euler angle (intrinsic)
+    '''Deprecated ...
+    Rotation matrix for Euler angle (intrinsic)
     v_world = Rrpy * v_body
 
 
@@ -123,7 +369,8 @@ def Cb2g(r,p,y):
 
 
 def T(r,p,y, tx,ty,tz):
-    ''' transform matrix  (rotation + translation)
+    '''Deprecated ...
+    transform matrix  (rotation + translation)
     xyz_world = T * xyz_body
     '''
 
@@ -137,7 +384,8 @@ def T(r,p,y, tx,ty,tz):
 
 
 def Q2R(q):
-    ''' Quaternion to rotation matrix
+    '''Deprecated ...
+    Quaternion to rotation matrix
     q - ( w, x, y, z)
 
     ref: https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
@@ -152,7 +400,8 @@ def Q2R(q):
 
 
 def R2Q(R):
-    ''' Rotation matrix to quaternion
+    '''Deprecated ...
+    Rotation matrix to quaternion
 
     R - 3x3 rotation matrix
 
@@ -170,7 +419,8 @@ def R2Q(R):
 
 
 def R2RPY(R):
-    ''' Roataion matrix to roll, pitch, yaw (X-Y-Z)
+    '''Deprecated ...
+    Roataion matrix to roll, pitch, yaw (X-Y-Z)
 
     return numpy array [r,p,y]
 
@@ -194,7 +444,8 @@ def R2RPY(R):
 
 
 def Q2ThetaV(q):
-    ''' extract angle, unit vector from quaternion (w,x,y,z)
+    '''Deprecated ...
+    extract angle, unit vector from quaternion (w,x,y,z)
     return theta(rad), unit_vector
     '''
     if abs(q[0]-1.0) < 1e-8:
@@ -208,7 +459,8 @@ def Q2ThetaV(q):
         return theta_d2*2, unit_vector
 
 def ThetaV2Q(theta,v):
-    ''' quaternion from angle and unit vector
+    '''Deprecated ...
+    quaternion from angle and unit vector
     return quaternion (w,x,y,z)
     '''
     q = np.zeros(4)
@@ -219,6 +471,12 @@ def ThetaV2Q(theta,v):
     q[3] = np.sin(theta/2) * v[2]
 
     return q
+
+
+
+
+
+
 
 
 def SPH2XYZ(rng,rho,phi):
@@ -254,49 +512,6 @@ def get_ground_range(rho, phi, T, ground_height=0.0):
 
 
 
-
-
-
-''' New Interfaces 
-    2017.12.08
-    TODO: add test funtions
-'''
-
-def RotationMatrixFromAxisAngle(axis, theta):
-    ''' return 3x3 Rotation Matrix'''
-    return Rxyz(theta, axis)
-
-def RotationMatrixFromEulerAngles(r,p,y):
-    ''' return 3x3 Rotation Matrix'''
-    return Rrpy(r,p,y)
-
-def RotationMatrixFromQuaternion(q):
-    ''' return 3x3 Rotation Matrix'''
-    return Q2R(q)
-
-
-def QuaternionFromRotationMatrix(R):
-    ''' return [w,x,y,z] '''
-    return R2Q(R)
-
-def QuaternionFromAxisAngle(v, theta):
-    ''' return [w,x,y,z] '''
-    return ThetaV2Q(theta, v)
-
-
-def AxisAngleFromQuaternion(q):
-    ''' return unit_vector, theta'''
-    t,u = Q2ThetaV(q)
-    return u,t
-
-def EulerAnglesFromRotationMatrix(R):
-    ''' return roll, pitch, yaw in radians'''
-    return R2RPY(R)
-
-
-def TransformationMatrix(r,p,y, tx,ty,tz):
-    ''' return 4x4 Transformation Matrix'''
-    return T(r,p,y, tx,ty,tz)
 
 
 
@@ -337,6 +552,8 @@ if __name__=='__main__':
     assert np.all(np.abs(unitv-u)<epsilon)
 
 
+
+
     ''' New Interfaces '''
     R1 = RotationMatrixFromAxisAngle('x', np.pi/2)
     R2 = np.array([[ 1,0,0], [0,0,-1], [0,1,0]], dtype=np.float)
@@ -350,21 +567,31 @@ if __name__=='__main__':
     R2 = np.array([[ 0,-1,0], [1,0,0], [0,0,1]], dtype=np.float)
     assert np.all(np.abs(R1-R2)<epsilon)
 
-
     R1 = RotationMatrixFromAxisAngle('x', np.pi/2)
-    # print(R1)
-    # print(R1-Q2R(R2Q(R1)))
-    assert np.all(np.abs(R1-Q2R(R2Q(R1)))<epsilon)
-
+    assert np.all(np.abs(R1-RotationMatrixFromQuaternion(QuaternionFromRotationMatrix(R1)))<epsilon)
 
     theta = np.radians(10)
     unitv = np.array([1,0,0])
     q = QuaternionFromAxisAngle(unitv, theta)
     u,t = AxisAngleFromQuaternion(q)
-    # print(theta, t)
-    # print(unitv, u)
     assert abs(theta-t)<epsilon
     assert np.all(np.abs(unitv-u)<epsilon)
 
 
+    R1 = RotationMatrixFromEulerAngles(np.pi/2, np.pi/2, np.pi/2)
+    Rx = np.array([[ 1,0,0], [0,0,-1], [0,1,0]], dtype=np.float)
+    Ry = np.array([[ 0,0,1], [0,1,0], [-1,0,0]], dtype=np.float)
+    Rz = np.array([[ 0,-1,0], [1,0,0], [0,0,1]], dtype=np.float)
+    R2 = np.dot(Rz, np.dot(Ry, Rx))
+    assert np.all(np.abs(R1-R2)<epsilon)
+
+
+    rpy1 = [0.1, 0.2, 0.3]
+    R1 = RotationMatrixFromEulerAngles(*rpy1)
+    rpy2 = EulerAnglesFromRotationMatrix(R1)
+    assert np.all(np.abs(rpy2-rpy1)<epsilon)
+
+
+    # TransformationMatrix(r,p,y, tx,ty,tz):
+    # TODO
 
